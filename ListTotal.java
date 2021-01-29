9c53e5e5fa82b7ef1ff233139e4b29e5e59eab6c
Tomas Oh <600018491@fjuhsd.org>
2021-01-29 17:38:44 +0000

    Added Unit 7 Lab 1

--- ListTotal.java
+++ ListTotal.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Tomas
+//Date - 01/29/2021
 
 import java.util.List;
 import java.util.ArrayList;
@@ -10,6 +10,18 @@ public class ListTotal
 {
  public static int total( List<Integer> ray )
  {
-  return 0;
+  //Create an int variable called sum, set equal to 0
+  int sum = 0;
+
+  //Using a for loop, iterate through all of the numbers in the ArrayList, using int counter = 0 as initializer and count++ as the update
+  for(int counter = 0; counter < ray.size(); counter++)
+  {
+    //In the loop body, set sum equal to sum plus the number in position counter of the ArrayList
+    sum = sum + ray.get(counter);
+
+    //Enclose the for-loop and return sum
+  }
+  return sum;
+  //Enclose the method
  }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
-//Date -
+//Name - Tomas Oh
+//Date - 01/29/2021
 
 import java.util.Arrays;
 
